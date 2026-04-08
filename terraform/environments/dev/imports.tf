db89b07c3d5156e586342ef3bfe506d2584f6fee
Alex Laird <contact@alexlaird.com>
2026-04-07 19:49:10 -0500

    Add TF import files for CloudWatch log groups
    Add one-time Terraform import files for dev and prod environments to bring pre-existing CloudWatch log groups under Terraform management. Each file imports module.ecs.aws_cloudwatch_log_group.platform using the existing IDs (/ecs/helium_platform_dev and /ecs/helium_platform_prod). These import blocks are only needed for the initial import and can be removed after the first successful apply.

---
+++ terraform/environments/dev/imports.tf
@@ -0,0 +1,7 @@
+# One-time imports for resources that existed before being brought under Terraform management.
+# These are safe to remove after the first successful apply.
+
+import {
+  to = module.ecs.aws_cloudwatch_log_group.platform
+  id = "/ecs/helium_platform_dev"
+}
