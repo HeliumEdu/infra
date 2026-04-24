73b5b4c43e15b076ac588282465ca7dc171ba1db
Alex Laird <contact@alexlaird.com>
2026-04-24 15:43:25 -0500

    Add backup_window to RDS instance
    Set backup_window on the aws_db_instance resource to "05:00-05:30" to schedule automated backups during a defined off-peak window. This ensures backups occur at a predictable time and can help reduce impact on peak traffic.

--- terraform/modules/environment/rds/main.tf
+++ terraform/modules/environment/rds/main.tf
@@ -30,5 +30,6 @@ resource "aws_db_instance" "helium" {
   vpc_security_group_ids     = [var.mysql_sg]
   db_subnet_group_name       = aws_db_subnet_group.helium.name
   multi_az                   = var.multi_az
+  backup_window              = "05:00-05:30"
   maintenance_window         = "sat:06:00-sat:07:00"
 }
