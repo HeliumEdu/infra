7cd83051a27034f7bb8ddc9f3104af45b41be38e
Alex Laird <contact@alexlaird.com>
2026-05-26 13:28:16 -0500

    Add rewrite for support-patreon.png
    Add mapping for '/img/support-patreon.png' in CloudFront rewrites to point to 'https://landing.heliumedu.com/img/support-patreon.png', ensuring the PNG asset is served alongside the existing SVG variant.

--- terraform/modules/environment/cloudfront/rewrites.js
+++ terraform/modules/environment/cloudfront/rewrites.js
@@ -20,6 +20,7 @@ function handler(event) {
         '/img/helium-logo.png': 'https://landing.heliumedu.com/img/helium-logo.png',
         '/img/helium-logo-square.png': 'https://landing.heliumedu.com/img/helium-logo-square.png',
         '/img/support-patreon.svg': 'https://landing.heliumedu.com/img/support-patreon.svg',
+        '/img/support-patreon.png': 'https://landing.heliumedu.com/img/support-patreon.png',
         '/.well-known/apple-app-site-association': 'https://landing.heliumedu.com/.well-known/apple-app-site-association',
         '/.well-known/assetlinks.json': 'https://landing.heliumedu.com/.well-known/assetlinks.json'
     };
