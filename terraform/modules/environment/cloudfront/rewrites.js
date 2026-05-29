43651692c44792dc92cb2269bfa9011face7bad7
Alex Laird <contact@alexlaird.com>
2026-05-29 00:14:31 -0500

    Add Microsoft identity association redirect
    Add a CloudFront rewrite mapping for /.well-known/microsoft-identity-association.json to point to landing.heliumedu.com so the Microsoft identity association file is served from the landing domain. Minor object punctuation adjusted to include the new entry.

--- terraform/modules/environment/cloudfront/rewrites.js
+++ terraform/modules/environment/cloudfront/rewrites.js
@@ -22,7 +22,8 @@ function handler(event) {
         '/img/support-patreon.svg': 'https://landing.heliumedu.com/img/support-patreon.svg',
         '/img/support-patreon.png': 'https://landing.heliumedu.com/img/support-patreon.png',
         '/.well-known/apple-app-site-association': 'https://landing.heliumedu.com/.well-known/apple-app-site-association',
-        '/.well-known/assetlinks.json': 'https://landing.heliumedu.com/.well-known/assetlinks.json'
+        '/.well-known/assetlinks.json': 'https://landing.heliumedu.com/.well-known/assetlinks.json',
+        '/.well-known/microsoft-identity-association.json': 'https://landing.heliumedu.com/.well-known/microsoft-identity-association.json'
     };
     if (sharedAssetRedirects[uri] !== undefined) {
         return {
