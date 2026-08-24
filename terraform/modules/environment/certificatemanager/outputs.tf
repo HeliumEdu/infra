7d3ddda6e6f2c1b0fa9b05e02b7a8c1562560192
Alex Laird <contact@alexlaird.com>
2026-08-24 18:30:11 -0500

    Update outputs.tf

--- terraform/modules/environment/certificatemanager/outputs.tf
+++ terraform/modules/environment/certificatemanager/outputs.tf
@@ -1,7 +1,7 @@
 output "heliumedu_com_cert_arn" {
-  value = aws_acm_certificate.heliumedu_com.arn
+  value = aws_acm_certificate_validation.heliumedu_com_cert_validation.certificate_arn
 }
 
 output "heliumstudy_com_cert_arn" {
-  value = aws_acm_certificate.heliumstudy_com.arn
+  value = aws_acm_certificate_validation.heliumstudy_com_cert_validation.certificate_arn
 }
